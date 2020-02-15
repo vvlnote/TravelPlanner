786543beb5c0a09b1bf4562f88f1e2c3ece23e58
Vivvie Lin <vvlnote@hotmail.com>
2020-02-14 22:53:55 -0800

    .env keeps the connection string to the remote MongoDB, and update server.js to establish the connection to the remote MongoDB

--- server.js
+++ server.js
@@ -2,11 +2,26 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
+
+
+
+
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, 
+{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully!');
+})
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
