eed7d150dc488a5d148479b3e3502a41f87d3fe2
Vivvie Lin <vvlnote@hotmail.com>
2020-02-14 17:10:11 -0800

    first commit, setting up the express server running on port 5000

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
