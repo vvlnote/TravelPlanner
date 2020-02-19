3a9c253d0b8209b870ddf150f5b2bfc8dff2cace
Vivvie Lin <vvlnote@hotmail.com>
2020-02-19 15:55:52 -0800

    Add <Provider> in the App.js, so all the components, and containers under app can access the state that store in the store. create the authReducer, and errorReducer to handle register and signin.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
           <h1>Hello</h1>
       </div>
+      </Provider>
     );
   }
 }
