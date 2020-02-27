d126e657d4c12ec8b62a5b024519032f169a3370
Vivvie Lin <vvlnote@hotmail.com>
2020-02-26 16:01:17 -0800

    add register function to handle new user to register to the app.

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,72 @@
+import axios from 'axios';
+import { returnErrors } from './errorActions';
+
+import {
+    USER_LOADED,
+    USER_LOADING,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT_SUCCESS,
+    REGISTER_SUCCESS,
+    REGISTER_FAIL
+} from './types';
+
+export const loadUser = () => (dispatch, getState) => {
+    //user loading
+    dispatch({ type: USER_LOADING });
+    
+    axios.get('/api/auth/user',tokenConfig(getState))
+        .then(res => dispatch({
+            type: USER_LOADED,
+            payload: res.data
+        })).catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+            dispatch({
+                type: AUTH_ERROR
+            })
+        })
+
+
+}
+
+export const register = ({ name, email, password }) => dispatch => {
+
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    const body = JSON.stringify({ name, email, password });
+
+    axios.post('/api/users', body, config)
+        .then(res => dispatch({
+            type: REGISTER_SUCCESS,
+            payload: res.data
+        })).catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status, 'REGISTER_FAIL'));
+            dispatch({
+                type: REGISTER_FAIL
+            })
+        });
+}
+
+
+//Setup config/header and token
+export const tokenConfig = getState => {
+    //Get token from loaclstorage
+    const token = getState().auth.token;
+
+    //headers
+    const config = {
+        headers: {
+            "Content-type": "application/json"
+        }
+    }
+
+    if (token) {
+        config.headers['x-auth-token'] = token
+    }
+
+    return config;
+}
